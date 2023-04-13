7e1fe0bd488ac0717a86206dcf31837625a095bf
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-13 14:38:04 +0000

    new

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,18 +1,29 @@
 import { NextComponentType } from "next"
 import Style from "@/scss/home.module.scss"
+import { gsap } from "gsap"
 
 const Home: NextComponentType = () => {
 
-   
+    const titles:any = gsap.utils.toArray('span')
+    const tl = gsap.timeline()
+    titles.forEach((title:any) => {
+        const text:any = new SplitText(title)
+        console.log(text)
+    })
 
     return(
         <div className={Style.home}>
             <div className={Style.box}>
                 <h1 className={Style.logo}>Fullzer4</h1>
             </div>
-            <div className={Style.box}>
-                <p>Hi there, my name is Gabriel Pelizzaro and Im a</p>
-                <div></div>
+            <div className={Style.Textbox}>
+                <p>Hi there, my name is Gabriel Pelizzaro and Im a 
+                    <div className={Style.Flowbox}>
+                        <span>FullStack developer</span>
+                        <span>ML and DL enginner</span>
+                        <span>Robotics</span>
+                    </div>
+                </p>
             </div> 
             <div className={Style.boxmouse}>
                 <svg width="40px" height="100%" viewBox="0 0 247 390" version="1.1" xmlns="http://www.w3.org/2000/svg" className={Style.mouse}>
