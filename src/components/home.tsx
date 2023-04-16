98017f3458667496bef4369aa8238c90b19c5aa1
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-15 23:43:28 -0300

    new

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,24 +1,10 @@
 import { NextComponentType } from "next"
 import Style from "@/scss/home.module.scss"
 import { useState } from "react"
+import Typical from "react-typical"
 
 const Home: NextComponentType = () => {
 
-    const [path, setPath] = useState("FullStack developer")
-
-    const animation = () => {
-       setTimeout(() => {
-        if(path === "FullStack developer"){
-            setPath("ML and DL enginnner")
-        }else if(path === "ML and DL enginnner"){
-            setPath("Robotics Engineer")
-        }else if(path === "Robotics Engineer"){
-            setPath("FullStack developer")
-        }
-       },3500)
-    }
-
-    animation()
 
     return(
         <div className={Style.home}>
@@ -26,7 +12,7 @@ const Home: NextComponentType = () => {
                 <h1 className={Style.logo}>Fullzer4</h1>
             </div>
             <div className={Style.Textbox}>
-                <p>Hi there, my name is Gabriel Pelizzaro and Im a <span>{path!}</span> </p>
+                <p>Hi there, my name is Gabriel Pelizzaro and Im a <Typical loop={Infinity} wrapper="b" steps={['Fullstack Developer', 2000, 'ML/DL Engineer', 2000, 'Robotics Engineer', 2000]}></Typical></p>
             </div> 
             <div className={Style.boxmouse}>
                 <svg width="40px" height="100%" viewBox="0 0 247 390" version="1.1" xmlns="http://www.w3.org/2000/svg" className={Style.mouse}>
