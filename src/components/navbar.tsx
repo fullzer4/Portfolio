187cff74f054a5cb987fe43478cc274ae540373a
fullzer4 <gabrielpelizzaro@gmail.com>
2023-05-29 09:38:43 -0300

    setup

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { NextComponentType } from "next"
+import Styles from "../scss/navbar.module.scss"
+
+const Navbar: NextComponentType = () => {
+  return(
+    <div className={Styles.navbar}>
+      
+    </div>
+  )
+}
+
+export default Navbar
