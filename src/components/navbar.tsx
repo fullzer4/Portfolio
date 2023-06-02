22b1137c03177f967e8e2c09a30aeef3b8af6a0a
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-02 11:09:36 -0300

    new

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,10 +5,15 @@ import Link from "next/link"
 const Navbar: NextComponentType = () => {
   return(
     <div className={Styles.navbar}>
-      <Link href="#Home" className={Styles.item}> Home </Link>
-      <Link href="#" className={Styles.item}> About </Link>
-      <Link href="#" className={Styles.item}> Projects </Link>
-      <Link href="#" className={Styles.item}> Talks </Link>
+      <div className={Styles.links}>
+        <Link href="#Home" className={Styles.item}> Home </Link>
+        <Link href="#" className={Styles.item}> About </Link>
+        <Link href="#" className={Styles.item}> Projects </Link>
+        <Link href="#" className={Styles.item}> Talks </Link>
+      </div>
+      <div className={Styles.logo}>
+        <p>Fullzer4</p>
+      </div>
     </div>
   )
 }
