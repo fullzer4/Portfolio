9804d3719f4ba37b53a1f53b9e82f9ba23d390fe
fullzer4 <gabrielpelizzaro@gmail.com>
2023-10-18 11:22:31 -0300

    fixes && inicia prod setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    https: true,
   },
-  plugins: [react(), mkcert()],
+  plugins: [react()],
 })
